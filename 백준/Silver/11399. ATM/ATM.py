9653c1bf6d04e3a73ae97c8f4658d33852adfd2c
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-02-03 20:29:08 +0900

    [Silver IV] Title: ATM, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,11 +1,11 @@
 N = int(input())
 
 Ps= list(map(int,input().split()))
-arr = []
+
 Ps.sort()
 ans = 0
+
 for i in range(1,N+1) :
-    for j in range(i) :
-        ans += Ps[j]
-        
+    ans += Ps[-i]*(i)
+
 print(ans)
