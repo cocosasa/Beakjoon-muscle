c4b24cd5902885e29aa24a3d47fcca68ffd0b92c
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-02-05 16:26:50 +0900

    [Bronze III] Title: 직각삼각형, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.py
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -1,11 +1,10 @@
 while True :
     legs = list(map(int,input().split()))
-    max_l = max(legs)
     legs.sort()
-    if max_l == 0 :
+    if legs[-1] == 0 :
         break
 
-    if legs[0]**2 + legs[1]**2 == max_l**2 :
+    if legs[0]**2 + legs[1]**2 == legs[2]**2 :
         print('right')
     else :
         print('wrong')
