7ebeca9a96893275e764923f04eb8313db0e6f0a
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-02-08 21:48:20 +0900

    [D2] Title: 스도쿠 검증, Time: 154 ms, Memory: 58,764 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,29 +1,31 @@
 T = int(input())
-
-test_arr = [1,2,3,4,5,6,7,8,9]
-
+ 
+test_set = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+ 
 for test_case in range(1, T + 1):
-    arr = [] 
-    tempbox = []
-    isgood = True
-    for i in range(9) :
-        arr.append(list(map(int,input().split())))
-    print(f'#{test_case}',end=" ")
-    for i in range(9) :
+    arr = []
+    box = []
+    flag = True
+    for i in range(9):
+        arr.append(list(map(int, input().split())))
+ 
+    for i in range(9):
         temp = []
-        if set(test_arr) != set(arr[i]) :
-                isgood = False
-        for j in range(9) :
-            
+        if test_set != set(arr[i]):
+            flag = False
+            break
+        for j in range(9):
             temp.append(arr[j][i])
-            tempbox.append(arr[i%3*3 + j//3 ][i%3*3 + j%3])
-            
-        if set(test_arr) != set(temp) :
-            isgood = False
-        if  set(test_arr) != set(tempbox)  :
-            isgood = False
-
-    if isgood :
-        print("1")
-    else :
-        print("0")
+            box.append(arr[i // 3 * 3 + j // 3][i % 3 * 3 + j % 3])
+ 
+        if test_set != set(temp):
+            flag = False
+            break
+        if test_set != set(box):
+            flag = False
+            break
+ 
+    if flag:
+        print(f'#{test_case} 1')
+    else:
+        print(f'#{test_case} 0')
