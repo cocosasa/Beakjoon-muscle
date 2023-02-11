031935dd66a453e300494ba93e474f6de2b5d17c
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-02-11 15:18:20 +0900

    [Silver I] Title: Z, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -1,29 +1,21 @@
 def rec(n,r,c,cnt) :
-    m = (2**(n-1))**2
-    mid = (2**n )/2
-    if n == 1 :
-        if r == 0 and c == 0 :
-            return cnt 
-        elif r == 0 and c == 1 :
-            return cnt + 1
-        elif r == 1 and c == 0 :
-            return cnt + 2
-        elif r == 1 and c == 1 :
-            return cnt + 3
+    mid = n//2
+    m = mid*mid
+    if n == 2 :
+        return cnt + r*2+c
     if r < mid:
         if c < mid :
-            ans  = rec(n-1,r,c,cnt)
+            ans  = rec(mid,r,c,cnt)
         else :
-            ans  = rec(n-1,r,c-mid,cnt+m)
+            ans  = rec(mid,r,c-mid,cnt+m)
     else :
         if c < mid :
-            ans  = rec(n-1,r-mid,c,cnt+2*m)
+            ans  = rec(mid,r-mid,c,cnt+2*m)
         else :
-            ans  = rec(n-1,r-mid,c-mid,cnt+3*m)
+            ans  = rec(mid,r-mid,c-mid,cnt+3*m)
     
     return ans
 N, r, c = map(int,input().split())
 
-
-ans = rec(N,r,c,0)
+ans = rec(2**N,r,c,0)
 print(ans)
