72d19e6d35f0502233cd5171f61b0ebfd582a115
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-02-18 14:41:41 +0900

    [Silver II] Title: 유기농 배추, Time: 324 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,20 +1,19 @@
 from collections import deque
 
 def bfs(y,x): 
-    if Map[y][x] != 1 :
-        return
     
     global land
     
     pueue = deque()
     
     pueue.append((y,x))
+    # 새로운 구역으로 식별
     Map[y][x] = land
     while pueue :
         yy , xx = pueue.popleft()
-        for n in range(4) :
-            ny, nx = yy + d[n][0],xx+d[n][1]
-            
+        for dy, dx in d :
+            ny, nx = yy + dy, xx+dx
+            #인덱스 체크
             if 0<=nx<M and 0<=ny<N :
                 if Map[ny][nx] == 1 :
                     pueue.append((ny,nx))
@@ -32,12 +31,16 @@ for tc in range(T) :
     Map = [[0]*M for _ in range(N)]
     for y,x in vechu :
         Map[y][x] = 1
-    land = 2
+    # 배추 정보를 2차원 배열에 매핑
+    
+    land = 2   # 카운트와 구역 구별을 위한 변수
 
-    d = [[0,1],[1,0],[0,-1],[-1,0]]
-    for i in range(N) :
-        for j in range(M) :
-            bfs(i,j)
-
+    d = [[0,1],[1,0],[0,-1],[-1,0]]  #델타검색을 위한 좌표증감
+    
+    # 모든 좌표에서 함수 호출
+    for i in range(N) :      
+        for j in range(M) :      
+            if Map[i][j] == 1 :
+                bfs(i,j)
 
     print(land - 2)
