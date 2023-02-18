2c9952f7706a3dfacd7f201e495c107223ff0496
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-02-18 16:17:15 +0900

    [Silver I] Title: 부등호, Time: 2080 ms, Memory: 524244 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.py
+++ 백준/Silver/2529. 부등호/부등호.py
@@ -6,10 +6,10 @@ boo = input().split()
 
 numbers = [0,1,2,3,4,5,6,7,8,9]
 
-pm = permutations(numbers,k+1)
-
-ans_list = []
-for p in pm :
+pm = list(permutations(numbers,k+1))
+len_pm = len(pm)
+for j in range(len_pm) :
+    p = pm[j]
     for i in range(k):
         if boo[i] == '<' :
             if p[i] < p[i+1] :
@@ -22,10 +22,24 @@ for p in pm :
             else :
                 break
     else :
-        ans_list.append(p)
-ans_list.sort()
-_max = max(ans_list)
-_min = min(ans_list)
+        _min = p
+        break
+for j in range(len_pm-1,-1,-1) :
+    p = pm[j]
+    for i in range(k):
+        if boo[i] == '<' :
+            if p[i] < p[i+1] :
+                continue
+            else :
+                break
+        else :
+            if p[i] > p[i+1] :
+                continue
+            else :
+                break
+    else :
+        _max = p
+        break
 
 print(*_max, sep='')
 print(*_min,sep='')
