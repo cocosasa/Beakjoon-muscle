2d2d2b37130680ec984d221631cd4d984ae424e4
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-02-22 13:33:09 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 3,590 ms, Memory: 150,884 KB -BaekjoonHub

--- SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.py
@@ -7,15 +7,13 @@ for tc in range(1, T + 1):
     ans = []
     _max = -1
     for i in range(N):
-        for j in range(i+1,N):
-            ans.append(arr[i] * arr[j])
-
-    for num in ans:
-        listnum = list(map(int,str(num)))
-        for i in range(0,len(listnum)-1) :
-            if listnum[i] > listnum[i+1] :
-                break
-        else :
-            if _max < num :
-                _max = num
+        for j in range(i + 1, N):
+            num = arr[i] * arr[j]
+            listnum = str(num)
+            for k in range(len(listnum) - 1):
+                if listnum[k] > listnum[k + 1]:
+                    break
+            else:
+                if _max < num:
+                    _max = num
     print(f'#{tc}', _max)
