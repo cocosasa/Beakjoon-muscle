9f26923fe6a9f4cb1740d0822724fa5d8c45323d
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-02-23 17:45:01 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산, Time: 261 ms, Memory: 66,296 KB -BaekjoonHub

--- SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.py
+++ SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.py
@@ -1,36 +1,25 @@
-def toint(c) :
-    if c.isdigit() :
+def toint(c):
+    if c.isdigit():
         return int(c)
     return c
-
-def postorder_cal(v) :
-    if tree[v][1] != 0 :
-        op = tree[v][0]
-        val1 = postorder_cal(tree[v][1])
-        val2 = postorder_cal(tree[v][2])
-
-        if op == '+' :
-            return val1 + val2
-        elif op == '-' :
-            return val1 - val2
-        elif op == '/' :
-            return val1 / val2
-        elif op == '*' :
-            return val1 * val2
+def postorder_cal(v):
+    if tree[v][1] != 0:
+        return op_cal[tree[v][0]](postorder_cal(tree[v][1]), postorder_cal(tree[v][2]))
     return tree[v][0]
-
-T = 10
-
-for tc in range(1,T+1) :
+op_cal = {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x / y,
+}
+for tc in range(1, 11):
     N = int(input())
-    tree = [[0]*3 for _ in range(N+1)]
-    for i in range(1,N+1) :
-        info = list(map(toint,input().split()))
-
-        tree[i][0] = info[1]
-        if len(info) > 2 :
-            tree[i][1] = info[2]
-            tree[i][2] = info[3]
-
-    ans = postorder_cal(1)
-    print(f'#{tc} {ans:.0f}')
+    tree = [[0] * 3 for _ in range(N + 1)]
+    for i in range(1, N + 1):
+        n, data, *child = map(toint, input().split())
+        v = tree[i]
+        v[0] = data
+        if child:
+            v[1] = child[0]
+            v[2] = child[1]
+    print(f'#{tc} {postorder_cal(1):.0f}')
