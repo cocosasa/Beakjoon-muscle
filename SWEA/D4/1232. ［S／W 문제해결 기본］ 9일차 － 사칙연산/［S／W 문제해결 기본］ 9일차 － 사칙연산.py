cfa0c90f19651113847487f3518240bce01eff4a
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-02-23 17:45:20 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산, Time: 249 ms, Memory: 64,964 KB -BaekjoonHub

--- SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.py
+++ SWEA/D4/1232. ［S／W 문제해결 기본］ 9일차 － 사칙연산/［S／W 문제해결 기본］ 9일차 － 사칙연산.py
@@ -2,16 +2,21 @@ def toint(c):
     if c.isdigit():
         return int(c)
     return c
+
+
 def postorder_cal(v):
     if tree[v][1] != 0:
         return op_cal[tree[v][0]](postorder_cal(tree[v][1]), postorder_cal(tree[v][2]))
     return tree[v][0]
+
+
 op_cal = {
     '+': lambda x, y: x + y,
     '-': lambda x, y: x - y,
     '*': lambda x, y: x * y,
     '/': lambda x, y: x / y,
 }
+
 for tc in range(1, 11):
     N = int(input())
     tree = [[0] * 3 for _ in range(N + 1)]
@@ -22,4 +27,5 @@ for tc in range(1, 11):
         if child:
             v[1] = child[0]
             v[2] = child[1]
+
     print(f'#{tc} {postorder_cal(1):.0f}')
