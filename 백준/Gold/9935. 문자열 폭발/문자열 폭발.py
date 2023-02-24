dd28e7aa96e0099b91949f708c485628376c27e5
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-02-24 15:27:19 +0900

    [Gold IV] Title: 문자열 폭발, Time: 548 ms, Memory: 49272 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -3,14 +3,14 @@ c4 = list(input())
 stack = []
 length = 0
 c4len = len(c4)
-for i in range(len(s)) :
-    stack.append(s[i])
+for c in s :
+    stack.append(c)
     length += 1
     if length >= c4len and stack[-c4len:] == c4:
         for i in range(c4len) :
             stack.pop()
             length -= 1
-if len(stack) :
+if length :
     print(''.join(stack))
 else :
     print('FRULA')
