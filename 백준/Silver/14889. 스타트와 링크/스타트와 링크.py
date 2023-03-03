1674d4e7785d41e42ef3abb65a2a0dc41606a1ab
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-03-03 17:10:13 +0900

    [Silver II] Title: 스타트와 링크, Time: 2720 ms, Memory: 57116 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -3,26 +3,27 @@ from itertools import combinations
 N = int(input())
 people = list(range(N))
 adj_matrix = [list(map(int, input().split())) for _ in range(N)]
-combi = list(combinations(people, N // 2))
+
+combi_lst = list(combinations(people, N // 2))
 
 _min = 9999
-
-for com in combi[:len(combi) // 2]:
-    team2 = list(set(people) - set(com))
+# com : 조합 and 팀 1
+for team1 in combi_lst[:len(combi_lst) // 2]:   
+    team2 = list(set(people) - set(team1))    #
     team1_synergy = 0
     team2_synergy = 0
-    for i in com:
-        for person in com:
-            if person == i:
-                continue
-            team1_synergy += adj_matrix[i][person]
-    for i in team2:
+    # 
+    for player in team1:
+        for person in team1:
+            team1_synergy += adj_matrix[player][person] 
+    for player in team2:        
         for person in team2:
-            if person == i:
-                continue
-            team2_synergy += adj_matrix[i][person]
+            team2_synergy += adj_matrix[player][person]
 
     cal = abs(team1_synergy - team2_synergy)
+
     if _min > cal:
         _min = cal
+        if _min == 0 :
+            break
 print(_min)
