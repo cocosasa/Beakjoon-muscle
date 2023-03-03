650440316afe54d6e3ed2392deaeff09044f6c82
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-03-03 16:20:27 +0900

    [Silver IV] Title: 색종이 - 2, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
@@ -1,44 +1,26 @@
-from collections import deque
-
-def bfs(si, sj) :
-    global ans
-    Q = deque()
-    Q.append((si,sj))
-    arr[si][sj] = VISITED
-
-    while Q :
-        ii, jj = Q.popleft()
-
-        for di, dj in d :
-            ni, nj = ii + di, jj + dj
-            if ni < 0 or ni > PAPER-1 or nj < 0 or nj > PAPER-1 :
-                continue
-            if arr[ni][nj] < BLACK :
-                ans += 1
-            elif arr[ni][nj] == BLACK :
-                Q.append((ni,nj))
-                arr[ni][nj] = VISITED
-
-d = ((0,1),(1,0),(-1,0),(0,-1))
+d = ((0, 1), (1, 0), (-1, 0), (0, -1))
 
 N = int(input())
 PAPER = 103
-BLACK = 6
-VISITED = 7
 
-arr = [[0]*PAPER for _ in range(PAPER)]
+arr = [[0] * PAPER for _ in range(PAPER)]
 
-for _ in range(N) :
-    y, x = map(int,input().split())
-    for i in range(y+1,y+11) :
-        for j in range(x+1,x+11) :
-            arr[i][j] = BLACK
+for _ in range(N):
+    y, x = map(int, input().split())
+    for i in range(y + 1, y + 11):
+        for j in range(x + 1, x + 11):
+            arr[i][j] = 5
 
 ans = 0
 
-for i in range(PAPER) :
-    for j in range(PAPER) :
-        if arr[i][j] == BLACK :
-            bfs(i,j)
-
+for i in range(PAPER):
+    for j in range(PAPER):
+        if arr[i][j] == 5:
+            for di, dj in d:
+                ni, nj = i + di, j + dj
+                if ni > 102 or nj > 102 or ni < 0 or nj < 0:
+                    continue
+                if arr[ni][nj] == 5:
+                    continue
+                ans += 1
 print(ans)
