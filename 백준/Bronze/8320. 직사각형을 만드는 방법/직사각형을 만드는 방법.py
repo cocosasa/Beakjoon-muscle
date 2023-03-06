c12c3e5c591d557dcf40fb2f9875d13bf1a854c0
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-03-06 09:34:03 +0900

    [Bronze II] Title: 직사각형을 만드는 방법, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.py
+++ 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.py
@@ -1,12 +1,7 @@
 N = int(input())
 ans = 0
-jeong = 0
 for i in range(1,N+1) :
-    for j in range(1,N+2 - i) :
-        tmp = i * j
-        if tmp <= N :
-            if i == j :
-                jeong += 1
-            else :
-                ans += 1
-print( ans//2 + jeong)
+    for j in range(i, N // i + 1) :
+        if i * j <= N :
+            ans += 1
+print( ans )
