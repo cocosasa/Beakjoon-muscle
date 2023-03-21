7af11c47fa03291a887b42bb1a046d13c4f22a66
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-03-21 10:13:58 +0900

    [Gold III] Title: 치즈, Time: 996 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.py
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -1,10 +1,12 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 N, M = map(int,input().split())
 
-arr = [list(map(int,input().split())) for _ in range(N) ]
+arr = [list(map(int,input().rstrip().split())) for _ in range(N) ]
 
 cnt = 0
-time = 2 # 2부터 시작하여 마지막에 2를 빼줌
+time = 0
 
 for i in range(N) :
     for j in range(M) :
@@ -17,20 +19,18 @@ while cnt> 0 :
     queue.append((0,0))
     while queue :
         ci, cj = queue.popleft()
-
         for di, dj in ((0,1),(1,0),(0,-1),(-1,0)) :
             ni,nj = ci + di, cj + dj
             if 0<=ni<N and 0<= nj < M : 
                 if arr[ni][nj] != 1 and visited[ni][nj] == 0 :
                     queue.append((ni,nj))
                     visited[ni][nj] = 5
-                    arr[ni][nj] = time
                 elif arr[ni][nj] == 1 :
                     visited[ni][nj] += 1
     for i in range(N) :
         for j in range(M) :
             if 1 < visited[i][j] < 5 :
-                arr[i][j] = time
+                arr[i][j] = 0
                 cnt -= 1
     time +=1
-print(time - 2)
+print(time)
