930813b7e81c0502a3c1286b53cdc6cc6604fa17
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-03-22 17:29:26 +0900

    [Gold III] Title: 내리막 길, Time: 164 ms, Memory: 42832 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,28 +1,24 @@
 def dfs(si, sj, last) :
     if si == N-1 and sj == M-1 :
         return 1
-    if possible[si][sj] == -1 :
-        return 0
     possible_cnt = 0
     for di, dj in d :
         ni, nj = si + di, sj + dj
         if 0<= ni < N and 0<= nj < M :
             if target <= road[ni][nj] < last :
-                if possible[ni][nj] > 0 :
+                if possible[ni][nj] >= 0 :
                     possible_cnt += possible[ni][nj]
                 else :
                     possible_cnt += dfs(ni, nj, road[ni][nj])
-    if possible_cnt > 0 :
-        possible[si][sj] = possible_cnt
-    else :
-        possible[si][sj] = -1
+    possible[si][sj] = possible_cnt
     return possible_cnt
-            
+
 N, M = map(int,input().split())
 
 road = [list(map(int,input().split())) for _ in range(N)]
-possible = [[0]*M for _ in range(N)]
+possible = [[-1]*M for _ in range(N)]
 d = ((0,1),(1,0),(0,-1),(-1,0))
 target = road[N-1][M-1]
+
 
 print(dfs(0,0,road[0][0]))
