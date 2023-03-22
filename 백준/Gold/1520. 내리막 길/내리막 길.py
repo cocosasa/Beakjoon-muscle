994ae5d74e74c76a28b06a9553fbecb63a865301
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-03-22 17:34:37 +0900

    [Gold III] Title: 내리막 길, Time: 136 ms, Memory: 42800 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,15 +1,18 @@
-def dfs(si, sj, last) :
+import sys 
+input = sys.stdin.readline
+
+def dfs(si, sj) :
     if si == N-1 and sj == M-1 :
         return 1
     possible_cnt = 0
     for di, dj in d :
         ni, nj = si + di, sj + dj
         if 0<= ni < N and 0<= nj < M :
-            if target <= road[ni][nj] < last :
+            if target <= road[ni][nj] < road[si][sj] :
                 if possible[ni][nj] >= 0 :
                     possible_cnt += possible[ni][nj]
                 else :
-                    possible_cnt += dfs(ni, nj, road[ni][nj])
+                    possible_cnt += dfs(ni, nj)
     possible[si][sj] = possible_cnt
     return possible_cnt
 
@@ -20,5 +23,4 @@ possible = [[-1]*M for _ in range(N)]
 d = ((0,1),(1,0),(0,-1),(-1,0))
 target = road[N-1][M-1]
 
-
-print(dfs(0,0,road[0][0]))
+print(dfs(0,0))
