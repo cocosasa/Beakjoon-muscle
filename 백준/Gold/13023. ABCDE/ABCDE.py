7c4ffb7976ad5ebfe7dc87c8b0dc41fce290f4df
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-04-02 22:04:25 +0900

    [Gold V] Title: ABCDE, Time: 896 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -0,0 +1,30 @@
+def dfs(k, last) :
+    global ans
+    if ans == 1 :
+        return
+    if k == 4 :
+        ans = 1
+        return 
+    for i in friend[last] :
+        if visit[i] == 0 :
+            visit[i] = 1
+            dfs(k+1, i)
+            visit[i] = 0
+
+n, m = map(int,input().split())
+
+friend = [[] for _ in range(n)]
+
+for i in range(m) :
+    a, b = map(int,input().split())
+    friend[a].append(b)
+    friend[b].append(a)
+
+ans = 0
+visit = [0]*n
+for start in range(n) :
+    visit[start] = 1
+    dfs(0,start)
+    visit[start] = 0
+
+print(ans)
