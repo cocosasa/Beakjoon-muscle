3fa49895e3d04ccb7e5ec192b3d23e9318026d54
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-04-05 18:00:29 +0900

    [Gold IV] Title: 빙산, Time: 2856 ms, Memory: 34264 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,67 +1,80 @@
 from collections import deque
-import sys; input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
-def bfs(si, sj) :
+# 섬하나의 ice개수를 count
+def bfs(si, sj):
     queue = deque()
-    queue.append((si,sj))
-    visited[si][sj] = land
-    
-    while queue :
-        i, j = queue.popleft()
+    queue.append((si, sj))
+    visited[si][sj] = ans
+    tmp_cnt = 1
+    while queue:
+        ci, cj = queue.popleft()
 
-        for di, dj in d :
-            ni, nj = i + di , j + dj 
-            if 0<= ni < row and 0<= nj < col and arr[ni][nj] and not visited[ni][nj] :
-                queue.append((ni,nj))
-                visited[ni][nj] = land
+        for di, dj in d:
+            ni, nj = ci + di, cj + dj
+            if 0 <= ni < row and 0 <= nj < col and arr[ni][nj] and visited[ni][nj] != ans:
+                queue.append((ni, nj))
+                visited[ni][nj] = ans
+                tmp_cnt += 1
+    return tmp_cnt
 
-row, col = map(int,input().split())
-arr = [list(map(int,input().split())) for _ in range(row)]
-d = ((0,1),(1,0),(0,-1),(-1,0))
+def land_check() :
+    land_cnt = 0
+    for i, j in ice_pos :
+        if arr[i][j] and visited[i][j] != ans:
+            land_cnt = bfs(i, j)
+            break
+    # 빙산 하나의 얼음이 남아있는 얼음의 개수와 다르다면 분리된 것
+    if land_cnt != ice:
+        return True
+    return False
+
+
+row, col = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(row)]
+visited = [[0]*col for _ in range(row)]
+d = ((0, 1), (1, 0), (0, -1), (-1, 0))
 
 ans = 1
+# 0이 아닌 값을 카운트
 ice = 0
-for i in range(row) :
-    for j in range(col) :
-        if arr[i][j] :
+ice_pos = []
+for i in range(row):
+    for j in range(col):
+        if arr[i][j]:
             ice += 1
-
-while ice :
-    melt = [[0]* col for _ in range(row)]
-    for i in range(row) :
-        for j in range(col) :
-            if arr[i][j] :
-                cnt = 0
-                for di, dj in d :
-                    ni, nj = i + di, j + dj
-                    if 0<= ni < row and 0<= nj < col and not arr[ni][nj] :
-                        cnt += 1
-                melt[i][j] = cnt
-    new_zero = False
-    for i in range(row) :
-        for j in range(col) :
-            if melt[i][j] :
-                next = arr[i][j] - melt[i][j]
-                if next <= 0 :
-                    arr[i][j] = 0
-                    new_zero = True
-                    ice -= 1
-                else :
-                    arr[i][j] = next
-    
-    land = 0
-    if new_zero :
-        visited = [[0]*col for _ in range(row)]
-        for i in range(row) :
-            for j in range(col) :
-                if arr[i][j] and not visited[i][j]:
-                    land += 1
-                    bfs(i,j)
-                    
-    if land >= 2 :
-        break
+            ice_pos.append((i,j))
+# 얼음이 남아있으면
+while ice:
+    # 녹을 얼음 계산
+    melt = []
+    for i, j in ice_pos :
+        if arr[i][j]:
+            cnt = 0
+            for di, dj in d:
+                ni, nj = i + di, j + dj
+                if 0 <= ni < row and 0 <= nj < col and not arr[ni][nj]:
+                    cnt += 1
+            if cnt:
+                melt.append((i, j, cnt))
+    # 얼음 녹음 , 0이 되는 얼음이 있는지 체크 
+    tmp = ice
+    if melt :
+        for i, j, cnt in melt:
+            next = arr[i][j] - cnt
+            if next <= 0:
+                arr[i][j] = 0
+                ice -= 1
+            else:
+                arr[i][j] = next
+    # 녹은 얼음이 있으면
+    if tmp != ice :
+        if land_check() :
+            break
     ans += 1
-if ice :
+
+if ice:
     print(ans)
-else :
+else:
     print(0)
