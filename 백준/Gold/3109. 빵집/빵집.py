fde94a289fbcfc1a4a9daa041354b97533f5a530
cocosasa <76679231+cocosasa@users.noreply.github.com>
2023-05-08 22:16:41 +0900

    [Gold II] Title: 빵집, Time: 2784 ms, Memory: 70084 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.py
+++ 백준/Gold/3109. 빵집/빵집.py
@@ -1,28 +1,23 @@
-from collections import deque
-
-def dfs(ci, cj) :
-    if cj == c-1 :
-        return True
-    arr[ci][cj] = 'x'
-    for di, dj in d :
-        ni, nj = ci + di, cj + dj
-        if arr[ni][nj] == '.' :
-            if dfs(ni,nj) :
-                return True
-    return False
-
 r, c = map(int,input().split())
 
 arr = [list('x'*c)]+[list(input()) for _ in range(r)]+[list('x'*c)]
 
-d = ((-1,1), (0,1), ( 1, 1))
-
-# print(arr)
+d = ((1,1), (0,1), ( -1, 1))
 
 ans = 0
 for i in range(1, r+1):
-    is_possible = dfs(i, 0)
-    if is_possible :
-        ans += 1
-            
+    stack = []
+    if arr[i][0] == 'x' :
+        continue
+    stack.append((i, 0))
+    while stack :
+        si, sj = stack.pop()
+        arr[si][sj] = 'x'
+        if sj == c-1 :
+            ans += 1
+            break
+        for di, dj in d :
+            ni, nj = si + di, sj + dj
+            if arr[ni][nj] == '.' :
+                stack.append((ni,nj))
 print(ans)
